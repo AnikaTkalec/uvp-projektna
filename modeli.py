641fdcbbad011751b447e0cae54361b3a53eae47
Anika Tkalec <tkalec.anika@gmail.com>
2022-09-20 14:53:48 +0200

    razred_pijaca

--- modeli.py
+++ modeli.py
@@ -50,5 +50,57 @@ def nalozi_natakarje():
                     najvecji_id = natakar['id']
             Natakar.stevec = najvecji_id + 1
                 
+class Pijaca:
+
+    stevec = None
+    pijace = {}
+
+
+    def __init__(self, tip_pijace, ime, kolicina, cena):
+        self.id = None
+        self.tip_pijace = tip_pijace
+        self.ime = ime
+        self.kolicina = kolicina
+        self.cena = cena
+
+    
+    def shrani(self):
+        if self.id is not None:
+            raise ValueError("self.id ni None, ne morem shraniti")
+        if Pijaca.stevec is None:
+            raise ValueError('Pijače še niso naložene')
+        self.id = Pijaca.stevec
+        Pijaca.stevec += 1
+        Pijaca.pijace[self.id] = self
+        with open("stanje/pijace.json", "w", encoding="UTF-8") as datoteka:
+            pijace = []
+            for pijaca in Pijaca.pijace.values():
+                pijace.append(
+                    {
+                        "id": pijaca.id,
+                        "tip_pijace": pijaca.tip_pijace,
+                        "ime": pijaca.ime,
+                        "kolicina": pijaca.kolicina,
+                        "cena": pijaca.cena,
+                    }
+                )
+            datoteka.write(json.dumps(pijace))
+
+def nalozi_pijace():
+    if not exists("stanje/pijace.json"):
+        Pijaca.stevec = 1
+    else:
+        with open("stanje/pijace.json", "r", encoding="UTF-8") as datoteka:
+            pijace = json.loads(datoteka.read())
+            najvecji_id = 0
+            for pijaca in pijace:
+                Pijaca.pijace[pijaca['id']] = Natakar(pijaca['tip_pijace'], pijaca['ime'], pijaca['kolicina'], pijaca['cena'])
+                Pijaca.pijace[pijaca['id']].id = pijaca['id']
+                if pijaca['id'] > najvecji_id:
+                    najvecji_id = pijaca['id']
+            Pijaca.stevec = najvecji_id + 1
+
+
+
 
 
